cfc80bfdad9ec543d3ca2c7bd76fa3dcaf04963f
Alexey Zabrodsky <elmigranto@gmail.com>
2017-04-02 01:47:21 +0500

    Implemented `UsermetaClient#read()`.

--- src/apis/UsermetaClient.js
+++ src/apis/UsermetaClient.js
@@ -2,11 +2,20 @@
 
 const urlEscape = require('url-escape-tag');
 const BaseClient = require('./BaseClient');
+const {toArray} = require('../utils');
 
 class UsermetaClient extends BaseClient {
   constructor ({protocol, hostname, port, pathnamePrefix, secret}) {
     super(`${protocol}://${hostname}:${port}${pathnamePrefix}`);
     this.secret = secret;
+  }
+
+  read (userIdsArg, metaNamesArg, callback) {
+    const userIds = toArray(userIdsArg).join(',');
+    const names = toArray(metaNamesArg).join(',');
+    const path = urlEscape`/${userIds}/${names}?secret=${this.secret}`;
+
+    this.apiCall('get', path, callback);
   }
 
   write (userId, metaName, value, callback) {
--- tests/apis/UsermetaClient.test.js
+++ tests/apis/UsermetaClient.test.js
@@ -3,14 +3,16 @@
 const UsermetaClient = require('../../src/apis/UsermetaClient');
 
 describe('UsermetaClient', () => {
+  const createClient = (secret) => new UsermetaClient({
+    protocol: 'https',
+    hostname: 'localhost',
+    port: 443,
+    pathnamePrefix: '/usermeta/v1',
+    secret
+  });
+
   it('#write()', (done) => {
-    const client = new UsermetaClient({
-      protocol: 'https',
-      hostname: 'localhost',
-      port: 443,
-      pathnamePrefix: '/usermeta/v1',
-      secret: 'secret'
-    });
+    const client = createClient('secret');
 
     td.replace(client.api, 'post', td.function());
 
@@ -21,6 +23,45 @@ describe('UsermetaClient', () => {
       expect(err).to.be.null;
       expect(res).to.eql({ok: true});
       done();
+    });
+  });
+
+  describe('#read()', () => {
+    it('reads single key', (done) => {
+      const client = createClient('secret');
+
+      td.replace(client.api, 'get', td.function());
+
+      td.when(client.api.get('/usermeta/v1/alice/key1?secret=secret'), td.callback)
+        .thenCallback(null, {}, {}, {alice: {key1: '1'}});
+
+      client.read('alice', 'key1', (err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.eql({alice: {key1: '1'}});
+        done();
+      });
+    });
+
+    it('reads multiple keys with proper escaping', (done) => {
+      const client = createClient('abc/00');
+
+      td.replace(client.api, 'get', td.function());
+
+      td.when(client.api.get('/usermeta/v1/alice%2Cjoe/key1%2Ckey2?secret=abc%2F00'), td.callback)
+        .thenCallback(null, {}, {}, {alice: {key1: '1', key2: '2'}, joe: {}});
+
+      client.read(['alice', 'joe'], ['key1', 'key2'], (err, res) => {
+        expect(err).to.be.eql(null);
+        expect(res).to.eql({
+          alice: {
+            key1: '1',
+            key2: '2'
+          },
+          joe: {}
+        });
+
+        done();
+      });
     });
   });
 });
