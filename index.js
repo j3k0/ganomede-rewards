97105492b16c9e65f29140c5d102aa1360deda45
Alexey Zabrodsky <elmigranto@gmail.com>
2017-04-02 02:13:13 +0500

    Split `index.js` to worker and server (inside a fork).

--- index.js
+++ index.js
@@ -1,92 +1,48 @@
 'use strict';
 
-const async = require('async');
-const cluster = require('cluster');
-const restify = require('restify');
 const curtain = require('curtain-down');
-const config = require('./config');
-const about = require('./src/about.router');
-const ping = require('./src/ping.router');
-const createServer = require('./src/create-server');
+const cluster = require('cluster');
+const serve = require('./src/serve');
+const work = require('./src/work');
 const logger = require('./src/logger');
+const config = require('./config');
 
-const master = () => {
-  let running = true;
-
-  curtain.on(() => {
-    if (!running) {
-      logger.info('aaaaaarghhhhh...');
-      return process.exit(1);
+const main = () => {
+  // Use New Relic if LICENSE_KEY has been specified.
+  if (process.env.NEW_RELIC_LICENSE_KEY) {
+    if (!process.env.NEW_RELIC_APP_NAME) {
+      const pk = require('./package.json');
+      process.env.NEW_RELIC_APP_NAME = pk.api;
     }
+    require('newrelic');
+  }
 
-    logger.info('master stopping…');
-    running = false;
+  if (cluster.isMaster) {
+    logger.info({config}, 'parsed config');
 
-    // Forcefully exit if workers hang for too long
-    setTimeout(process.exit.bind(process, 0), 30e3).unref();
-  });
+    // Start worker in master.
+    work();
 
-  logger.info(config, 'parsed config');
+    // Spawn HTTP server in secondary
+    // (so it can die on exceptions without killing worker).
+    let running = true;
+    curtain.once(() => running = false);
 
-  cluster.fork();
+    cluster.on('disconnect', (worker) => {
+      logger.info('worker disconnected');
 
-  cluster.on('disconnect', (worker) => {
-    logger.info('worker disconnected');
+      if (running) {
+        logger.error('restarting…');
+        cluster.fork();
+      }
+    });
 
-    if (running) {
-      logger.error('restarting…');
-      cluster.fork();
-    }
-  });
-};
-
-const child = () => {
-  const server = createServer();
-
-  curtain.on(() => {
-    logger.info('worker stopping…');
-
-    async.parallel([
-      (cb) => server.close(cb),
-    ], () => cluster.worker.disconnect());
-  });
-
-  about(config.http.prefix, server);
-  ping(config.http.prefix, server);
-
-  server.listen(config.http.port, config.http.host, () => {
-    const {port, family, address} = server.address();
-    logger.info('ready at %s:%d (%s)', address, port, family);
-  });
-
-  // Handle uncaughtException, kill the worker.
-  server.on('uncaughtException', (req, res, route, err) => {
-    logger.error(err);
-
-    // Note: we're in dangerous territory!
-    // By definition, something unexpected occurred,
-    // which we probably didn't want.
-    // Anything can happen now! Be very careful!
-    try {
-      // make sure we close down within 30 seconds
-      setTimeout(() => process.exit(1), 30e3);
-
-      // stop taking new requests
-      server.close();
-
-      // Let the master know we're dead.  This will trigger a
-      // 'disconnect' in the cluster master, and then it will fork
-      // a new worker.
-      cluster.worker.disconnect();
-
-      const message = err.message || 'unexpected error';
-      res.send(new restify.InternalError(message));
-    }
-    catch (err2) {
-      logger.error(err2, 'error sending 500!');
-    }
-  });
+    cluster.fork();
+  }
+  else {
+    serve();
+  }
 };
 
 if (!module.parent)
-  cluster.isMaster ? master() : child();
+  main();
---
+++ src/work.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const work = () => {
+  throw new Error('NotImplemented');
+};
+
+module.exports = work;
